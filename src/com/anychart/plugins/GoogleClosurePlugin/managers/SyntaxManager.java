e325dfb50f04b5dddb80b7dbdcebaba98c156535
chidori <kagakinam@gmail.com>
2011-12-04 01:47:02 +0900

    init

---
+++ src/com/anychart/plugins/GoogleClosurePlugin/managers/SyntaxManager.java
@@ -0,0 +1,9 @@
+package com.anychart.plugins.GoogleClosurePlugin.managers;
+
+/**
+ * Author: Anton Kagakin kagakinam@gmail.com
+ * Date: 04.12.11
+ * Time: 1:40
+ */
+public class SyntaxManager {
+}
