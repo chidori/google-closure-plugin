05680b0137f01cd68e72fd531cc725983f4e7043
Anton Saukh <grasshoppergn@gmail.com>
2011-12-04 02:37:26 +0900

    Interface for SyntaxManager + SyntaxManagerResult

--- src/com/anychart/plugins/GoogleClosurePlugin/managers/SyntaxManager.java
+++ src/com/anychart/plugins/GoogleClosurePlugin/managers/SyntaxManager.java
@@ -1,9 +1,20 @@
 package com.anychart.plugins.GoogleClosurePlugin.managers;
 
+import com.anychart.plugins.GoogleClosurePlugin.managers.syntax.SyntaxManagerResult;
+
 /**
- * Author: Anton Kagakin kagakinam@gmail.com
+ * Author: Anton Saukh saukham@gmail.com
  * Date: 04.12.11
  * Time: 1:40
  */
 public class SyntaxManager {
+    public SyntaxManager(ContentManager contentManager) {
+        this.contentManager = contentManager;
+    }
+
+    private ContentManager contentManager;
+
+    public SyntaxManagerResult parseInput(String inputString) {
+        return null;
+    }
 }
