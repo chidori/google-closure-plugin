9cbd1824b20c477d39b8116ee751846f9cc5d870
chidori <kagakinam@gmail.com>
2011-12-04 04:18:54 +0900

    Start to work with ContentManager

--- src/com/anychart/plugins/GoogleClosurePlugin/managers/ContentManager.java
+++ src/com/anychart/plugins/GoogleClosurePlugin/managers/ContentManager.java
@@ -1,19 +1,34 @@
 package com.anychart.plugins.GoogleClosurePlugin.managers;
 
+import com.intellij.openapi.editor.Document;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.editor.EditorGutter;
+import com.intellij.openapi.fileEditor.FileDocumentManager;
+import com.intellij.openapi.vfs.VirtualFile;
+
+import javax.swing.text.DocumentFilter;
+
+
 /**
  * Author: Anton Kagakin kagakinam@gmail.com
  * Date: 04.12.11
  * Time: 1:40
  */
 public class ContentManager {
-    public String getCurrentNamespace(){
+
+    public String getNamespace(Editor editor) {
         // use regexps like "goog\.provide\(([a-zA-Z.]+)\)", where you need the shortest $1 value
         return null;
     }
 
-    public String getCurrentClassName(){
+    public String getClassName(Editor editor) {
         // use regexps like "^((([a-z][a-zA-Z_]+)\.)+[A-Z][a-zA-Z]+)[ ]+=[ ]+function", where you need $3 value, to not
         // to be "prototype." string.
         return null;
     }
+
+    public void insertString(Document doc, String text, int offset) {
+
+    }
+
 }
