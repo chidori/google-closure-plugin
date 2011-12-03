929fb6293ba023e95e9ad15b657dd7d07a4521a0
chidori <kagakinam@gmail.com>
2011-12-04 05:40:31 +0900

    ContentManager

--- src/com/anychart/plugins/GoogleClosurePlugin/applicationActions/Insert.java
+++ src/com/anychart/plugins/GoogleClosurePlugin/applicationActions/Insert.java
@@ -1,5 +1,8 @@
 package com.anychart.plugins.GoogleClosurePlugin.applicationActions;
 
+import com.anychart.plugins.GoogleClosurePlugin.managers.ContentManager;
+import com.anychart.plugins.GoogleClosurePlugin.managers.SyntaxManager;
+import com.anychart.plugins.GoogleClosurePlugin.managers.syntax.SyntaxManagerResult;
 import com.intellij.codeInsight.template.Template;
 import com.intellij.codeInsight.template.TemplateEditingListener;
 import com.intellij.codeInsight.template.TemplateManager;
@@ -10,6 +13,9 @@ import com.intellij.openapi.actionSystem.AnActionEvent;
 import com.intellij.openapi.actionSystem.PlatformDataKeys;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.project.Project;
+import com.intellij.openapi.util.TextRange;
+
+import javax.xml.soap.Text;
 
 /**
  * Author: Anton Kagakin kagakinam@gmail.com
@@ -24,10 +30,8 @@ public class Insert extends AnAction {
     }*/
 
     public void actionPerformed(AnActionEvent e) {
-        System.out.println("KJDFJS:DJFKL:JSDF");
         TemplateImpl template = new TemplateImpl("", "$1$", "other");
 
-//        template.setString("$1$");
         template.setDescription("sync edit template");
         template.setToIndent(false);
         template.setToReformat(false);
@@ -37,17 +41,27 @@ public class Insert extends AnAction {
         template.addVariable("1", "", "", true);
 
         Project myProject = e.getData(PlatformDataKeys.PROJECT);
-        Editor editor = e.getData(PlatformDataKeys.EDITOR);
+        final Editor editor = e.getData(PlatformDataKeys.EDITOR);
         assert editor != null;
+
+        final int replaceFrom = editor.getCaretModel().getOffset();
+        final ContentManager contentManager = new ContentManager();
+        final SyntaxManager syntaxManager = new SyntaxManager(contentManager);
         TemplateManager templateManager = TemplateManager.getInstance(myProject);
-
-
 
         templateManager.startTemplate(editor, template, new TemplateEditingListener() {
             public void beforeTemplateFinished(TemplateState state, Template template) {
+
             }
 
             public void templateFinished(Template template, boolean brokenOff) {
+                int replaceTo = editor.getCaretModel().getOffset();
+                TextRange textRange = new TextRange(replaceFrom, replaceTo);
+                String myString = editor.getDocument().getText(textRange);
+                System.out.println(myString);
+                SyntaxManagerResult parsedString = syntaxManager.parseInput(myString);
+                contentManager.insertString(editor.getDocument(), "chidori", replaceFrom, replaceTo);
+//                editor.getDocument().replaceString(replaceFrom, replaceTo, parsedString.toString());
                 System.out.println("TEMPLATE FINISHED");
             }
 
--- src/com/anychart/plugins/GoogleClosurePlugin/managers/ContentManager.java
+++ src/com/anychart/plugins/GoogleClosurePlugin/managers/ContentManager.java
@@ -4,9 +4,12 @@ import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.EditorGutter;
 import com.intellij.openapi.fileEditor.FileDocumentManager;
+import com.intellij.openapi.util.TextRange;
 import com.intellij.openapi.vfs.VirtualFile;
 
 import javax.swing.text.DocumentFilter;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 /**
@@ -17,18 +20,62 @@ import javax.swing.text.DocumentFilter;
 public class ContentManager {
 
     public String getNamespace(Editor editor) {
+        VirtualFile file = FileDocumentManager.getInstance().getFile(editor.getDocument());
+
+        String text = editor.getDocument().getText();
+        String namespace;
+
+        int googProvideOffset = text.indexOf("goog.provide(", 0);
+        if (googProvideOffset != 0) {
+            namespace = text.substring(googProvideOffset, text.lastIndexOf(")", googProvideOffset) - 1);
+        } else {
+            String filePath = file.getPath();
+            String fileName = file.getName();
+            namespace = filePath.substring(
+                    filePath.indexOf("src/") + 4, filePath.indexOf(fileName) - 1
+            ).replaceAll("\\/", ".");
+
+            String cmp2 = fileName.substring(0, fileName.lastIndexOf("."));
+            String cmp1 = namespace.substring(namespace.lastIndexOf(".", namespace.length() - 1));
+            if (cmp1.equals(cmp2)) {
+                namespace += "." + cmp2;
+            }
+        }
+
         // use regexps like "goog\.provide\(([a-zA-Z.]+)\)", where you need the shortest $1 value
-        return null;
+        return namespace;
     }
 
     public String getClassName(Editor editor) {
+        String className = "";
+
+        int offset = editor.getCaretModel().getOffset();
+        int lineNumber = editor.getDocument().getLineNumber(offset);
+        Pattern p = Pattern.compile("^((([a-z][a-zA-Z_]+)\\.)+[A-Z][a-zA-Z]+)[ ]+=[ ]+function");
+
+        while (lineNumber != -1) {
+            int lineStartOffset = editor.getDocument().getLineStartOffset(lineNumber);
+            int lineEndOffset = editor.getDocument().getLineEndOffset(lineNumber);
+            TextRange textRange = new TextRange(lineStartOffset, lineEndOffset);
+            String stringLine = editor.getDocument().getText(textRange);
+
+            Matcher m = p.matcher(stringLine);
+
+            if (!m.matches()) {
+                lineNumber -= 1;
+            } else {
+                className = m.group(3);
+                return className;
+            }
+        }
+        if (className.equals("")) return null;
+        return className;
         // use regexps like "^((([a-z][a-zA-Z_]+)\.)+[A-Z][a-zA-Z]+)[ ]+=[ ]+function", where you need $3 value, to not
         // to be "prototype." string.
-        return null;
     }
 
-    public void insertString(Document doc, String text, int offset) {
-
+    public void insertString(Document doc, String text, int from, int to) {
+        doc.replaceString(from, to, text);
     }
 
 }
